27f0b9ffecfc216e73126826ac6b3e546a0ea069
Nftshad <103199681+nftshad@users.noreply.github.com>
2022-05-16 15:57:16 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
   API_URL: "rarity-score-peach.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
-  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+  COLLECTION_NAME: "nftshredder",
+  COLLECTION_TITLE: "NFT shredder",
+  COLLECTION_DESCRIPTION: '',
+  COLLECTION_IMG_LINK: "",
   STARTING_INDEX: 0,
 };
