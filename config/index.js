ea7234fcf08f4bb253e6291799c550bbbc0ce5ec
Nftshad <103199681+nftshad@users.noreply.github.com>
2022-05-18 09:10:22 +0300

    Change api url

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "rarity-score-peach.vercel.app",
+  API_URL: "nft-shredder.vercel.app",
   COLLECTION_NAME: "nftshredder",
   COLLECTION_TITLE: "NFT shredder",
   COLLECTION_DESCRIPTION: '',
